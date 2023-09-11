77be30719456144ee1ded1b036ae8023855829ab
Muntaha07 <128796437+Muntaha07@users.noreply.github.com>
2023-09-12 05:15:52 +0530

    dome

--- script.js
+++ script.js
@@ -62,6 +62,7 @@ reset.addEventListener('click', ()=>{
     isgameover = false
     document.querySelector(".line").style.width = "0vw";
     document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
 })
 
